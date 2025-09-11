706e778b19f7bbaea3c937c1b629d00a5ad15254
LeonidaMan <peterhakem787@gmail.com>
2025-09-11 20:53:31 +0300

    add intro book add server-ui for intro book add bluestone_dust item, its bit useless for now

--- bluestone-bp/scripts/main.js
+++ bluestone-bp/scripts/main.js
@@ -1,5 +1,28 @@
-import { world } from "@minecraft/server";
+import { system, world, ItemComponentTypes } from "@minecraft/server";
+import { ActionFormData } from "@minecraft/server-ui";
 
-world.afterEvents.worldLoad.subscribe(() => {
-  world.sendMessage("Hello, World!")
+/**
+ * @param {import("@minecraft/server").Player} source
+ */
+function introUi(source) {
+  const form = new ActionFormData()
+    .title({ translate: "bs:ui.title" })
+    .body({ translate: "bs:ui.body" })
+    .button({ translate: "bs:ui.exit" });
+
+  form.show(source).then((result) => {
+    if (result.canceled || result.selection === undefined) return;
+
+    console.log(result.selection);
+  });
+}
+
+system.beforeEvents.startup.subscribe(init => {
+  init.itemComponentRegistry.registerCustomComponent("bs:intro", {
+    onUse: ({ itemStack, source }) => {
+      const cooldown = itemStack.getComponent(ItemComponentTypes.Cooldown);
+      cooldown.startCooldown(source);
+      introUi(source)
+    }
+  })
 })
